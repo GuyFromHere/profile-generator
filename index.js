de900a4ae705da3748095f32af1c08f06810568b
Dan Dismuke <dandismuke@gmail.com>
2019-12-20 20:03:05 -0800

    More work on styles...

--- index.js
+++ index.js
@@ -29,67 +29,158 @@ The background color of the generated PDF matches the color that the user provid
 */
 
 inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your GitHub username?",
-      name: "username"
-    },
-    {
-      type: "input",
-      message: "What is your favorite color?",
-      name: "color"
-    }
-  ])
-  .then(function(response) {
-    const color = response.color;
-    axios
-      .get(`https://api.github.com/users/${response.username}`)
-      .then(res => {
-        const username = res.data.login;
-        const profile = res.data.html_url;
-        const followers = res.data.followers_url;
-        const bio = res.data.bio;
-        const pic = res.data.html_url + ".png";
-        // console.log(res.data);
-        console.log(profile);
-        console.log(followers);
-        console.log(bio);
-        console.log(pic);
+    .prompt([
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your favorite color?",
+            name: "color"
+        }
+    ])
+    .then(function (response) {
+        const color = response.color;
+        axios
+            .get(`https://api.github.com/users/${response.username}`)
+            .then(res => {
+                const username = res.data.login;
+                const profile = res.data.html_url;
+                const followers = res.data.followers_url;
+                const bio = res.data.bio;
+                const pic = res.data.html_url + ".png";
+                console.log(res.data);
 
-        const page = `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <title>Document</title>
-                <style>
-                    body {
-                        background-color: ${color};
-                        color: white;
-                    }
-                    a {
-                        text-decoration: none;
-                        color: white;
-                    }
-                </style>
-            </head>
-            <body>
-            <h1>${username}</h1>
-            <img src='${pic}'>
-            <ul>
-                <li><a href='${profile}' target='_blank'>GitHub Profile</a></li>
-            </ul>
-            </body>
-            </html>
+                const page = `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>${username}</title>
+      <style>
+          body {
+              font-family: "Noto Sans, sans-serif";
+          }
+  
+          .highlight {
+              background-color: ${color};
+              color: white;
+              text-align: center;
+              border-radius: 2rem;
+              font-family: "Noto Sans, sans-serif";
+          }
+  
+          .headerContainer {
+              background-color: grey;
+          }
+  
+          .header {
+              /* position: relative; */
+              /* font-family: "Noto Sans, sans-serif"; */
+              display: flex;
+              flex-direction: row;
+              /* width: 80%; */
+              /* right: 20%; */
+              /*  bottom: 20%; */
+              overflow: hidden;
+              padding: 30px;
+              border-radius: 2rem;
+          }
+  
+          .imageContainer {
+              position: relative;
+              border-radius: 50%;
+              /* left: -30%; */
+              /* overflow: hidden; */
+              /* text-align: center; */
+          }
+  
+          #profilePic {
+              border: 2px solid white;
+              width: 300px;
+              border-radius: 50%;
+          }
+  
+          a {
+              text-decoration: none;
+              color: black;
+          }
+  
+          li {
+              float: left;
+              list-style-type: none;
+              padding: 5px;
+              color: black;
+          }
+  
+  
+          .row {
+              display: flex;
+              flex-direction: row;
+              /* margin: auto 0; */
+          }
+  
+          .card {
+              height: 100px;
+              width: 200px;
+              border: 1px solid white;
+              border-radius: 5px;
+              padding: 20px;
+              margin: 10px;
+          }
+      </style>
+  </head>
+  
+  <body>
+  
+      <div class="headerContainer">
+          <div class="highlight header">
+              <div class="imageContainer">
+                  <img width="128" id="profilePic" src="${pic}" />
+                  <ul class="headerLinks">
+                      <li>Location</li>
+                      <li>
+                          <a href="${profile}" target="_blank">GitHub Profile</a>
+                      </li>
+                      <li>Blog</li>
+                  </ul>
+              </div>
+              <div>
+                  <h1>${username}</h1>
+                  <h4>Status here</h4>
+                  <span class="bio">${bio}</span>
+              </div>
+              <div class="main">
+  
+                  <div class="row">
+                      <div class="highlight card">GitHub Repositories</div>
+                      <div class="highlight card">GitHub Stars</div>
+                  </div>
+                  <div class="row">
+                      <div class="highlight card"><a href="${followers}" target="_blank">Followers</a></div>
+                      <div class="highlight card">Following</div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  
+  </body>
+  
+  </html>
             `;
-        fs.writeFile("index.html", page, err => {
-          if (err) throw err;
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+
+                fs.writeFile("index.html", page, err => {
+                    if (err) throw err;
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
+
+
