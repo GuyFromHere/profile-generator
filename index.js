c7239efa41a3debaf66c405c60a42f05bd3991f2
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2019-12-23 09:53:15 -0800

    More style work. More content. PDF conversion is functional with puppeteer.

--- index.js
+++ index.js
@@ -1,78 +1,68 @@
 const inquirer = require("inquirer");
 const axios = require("axios");
 const fs = require("fs");
-const generateHTML = require('./generateHTML.js')
+const generateHTML = require("./generateHTML.js").generateHTML;
+const puppeteer = require("puppeteer");
 
-const questions = [{
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "username"
-    },
-    {
-        type: "input",
-        message: "What is your favorite color?",
-        name: "color"
-    }
-]
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "What is your favorite color?",
+    name: "color"
+  }
+];
 
-const getRepos = function (url) {
-    axios.get(url).then(res => {
-            let x = 0;
-            /* res.data.forEach(item =>
-                x += item.watchers
-            ) */
-            res.data.forEach(element => {
-                console.log(element.name);
-                console.log(element.watchers);
-                x += element.watchers;
-            });
-            console.log('x = ' + x)
-            return x;
+inquirer.prompt(questions).then(function(response) {
+  /* const color = response.color; */
+  const color = "red";
+  axios
+    //.get(`https://api.github.com/users/${response.username}`)
+    .get(`https://api.github.com/users/guyfromhere`)
+    .then(res => {
+      const getData = {
+        color: color,
+        name: res.data.name,
+        username: res.data.login,
+        location: res.data.location,
+        profile: res.data.html_url,
+        followers: res.data.followers,
+        following: res.data.following,
+        bio: res.data.bio,
+        pic: res.data.html_url + ".png",
+        company: res.data.company,
+        blog: res.data.blog,
+        publicRepos: res.data.public_repos,
+        reposUrl: res.data.repos_url
+      };
+      //console.log(res);
+      axios
+        .get(getData.reposUrl)
+        .then(res => {
+          let counter = 0;
+          res.data.forEach(element => {
+            counter += element.watchers;
+          });
+          getData.watchers = counter;
+          const newPage = generateHTML(getData);
+          fs.writeFile("index.html", newPage, err => {
+            if (err) throw err;
+            (async () => {
+              const browser = await puppeteer.launch();
+              const page = await browser.newPage();
+              await page.goto(
+                "file:///C:/documents/github/profile-generator/index.html"
+              );
+              await page.pdf({ path: `${getData.username}.pdf`, format: "A4" });
+              await browser.close();
+            })();
+          });
         })
-        .catch(err =>
-            console.log(err))
-}
-
-inquirer
-    .prompt(questions)
-    .then(function (response) {
-        /* const color = response.color; */
-        const color = 'red';
-        axios
-            //.get(`https://api.github.com/users/${response.username}`)
-            .get(`https://api.github.com/users/guyfromhere`)
-            .then(res => {
-                /* const getData = {
-                    color: 'red',
-                    username: 'GuyFromHere',
-                    profile: 'https://github.com/GuyFromHere',
-                    followers: 'https://api.github.com/users/GuyFromHere/followers',
-                    bio: 'IT guy and aspiring web developer. Currently enrolled in UO\'s Full Stack Coding Bootcamp.',
-                    pic: 'https://github.com/GuyFromHere.png'
-                } */
-                const getData = {
-                    color: color,
-                    name: res.data.name,
-                    username: res.data.login,
-                    profile: res.data.html_url,
-                    followers: res.data.followers,
-                    following: res.data.following,
-                    starred: res.data.starred_url,
-                    bio: res.data.bio,
-                    pic: res.data.html_url + ".png",
-                    company: res.data.company,
-                    blog: res.data.blog,
-                    publicRepos: res.data.public_repos
-                }
-                //console.log(res);
-                let stars = getRepos('https://api.github.com/users/GuyFromHere/repos')
-                console.log('stars = ' + stars)
-                const newPage = generateHTML.generateHTML(getData)
-                fs.writeFile("index.html", newPage, err => {
-                    if (err) throw err;
-                });
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    });
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
