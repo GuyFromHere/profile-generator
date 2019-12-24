e52526b5d4bbdb04b17e527b3a510876d4044914
Dan Dismuke <47902660+GuyFromHere@users.noreply.github.com>
2019-12-23 16:52:56 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const generateHTML = require("./generateHTML.js").generateHTML;
 const puppeteer = require("puppeteer");
 const maps_api = require("./api.js").maps_api;
 
-console.log(maps_api);
 const questions = [
   {
     type: "input",
